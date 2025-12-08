4c7a97c63292bb5c84aa17feb72c8f49dfb86748
Willian Vagner Vicente Corrêa <willian.vag@gmail.com>
2025-12-08 20:55:36 -0300

    Add `temperature-processing` Spring Boot project with Gradle configuration
    - Configured Gradle wrapper and dependencies
    - Added starter application and test classes
    - Set up project structure and initial configuration

---
+++ temperature-processing/src/main/java/dev/williancorrea/sensors/temperature/processing/TemperatureProcessingApplication.java
@@ -0,0 +1,13 @@
+package dev.williancorrea.sensors.temperature.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureProcessingApplication.class, args);
+	}
+
+}
---
+++ temperature-processing/src/test/java/dev/williancorrea/sensors/temperature/processing/TemperatureProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.williancorrea.sensors.temperature.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
