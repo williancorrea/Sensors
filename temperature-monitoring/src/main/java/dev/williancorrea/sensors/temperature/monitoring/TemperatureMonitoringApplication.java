6babc5bbe36712b28dc308b1679875cd0a5774df
Willian Vagner Vicente Corrêa <willian.vag@gmail.com>
2025-12-08 20:55:00 -0300

    Add `temperature-monitoring` Spring Boot project with Gradle configuration
    - Configured Gradle wrapper and dependencies
    - Added starter application and test classes
    - Set up `.gitignore` and `.gitattributes` files

---
+++ temperature-monitoring/src/main/java/dev/williancorrea/sensors/temperature/monitoring/TemperatureMonitoringApplication.java
@@ -0,0 +1,13 @@
+package dev.williancorrea.sensors.temperature.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureMonitoringApplication.class, args);
+	}
+
+}
