063b952f08ad3b05a71c899af6c76718d1955681
Willian Vagner Vicente Corrêa <willian.vag@gmail.com>
2025-12-08 20:55:20 -0300

    Add `device-management` Spring Boot project with Gradle setup
    - Configured Gradle wrapper and dependencies
    - Added starter application and test classes
    - Set up project structure and initial configuration

---
+++ device-management/src/test/java/dev/williancorrea/sensors/device/management/DeviceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.williancorrea.sensors.device.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
