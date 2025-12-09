fcadbda008111b14054f3ba82b624c538355276d
Willian Vagner Vicente Corrêa <willian.vag@gmail.com>
2025-12-09 09:03:27 -0300

    Refactor `Sensor` entity to include `@Entity` annotation and introduce `SensorId` as a TSID-based embeddable ID class

--- device-management/src/main/java/dev/williancorrea/sensors/device/management/domain/model/Sensor.java
+++ device-management/src/main/java/dev/williancorrea/sensors/device/management/domain/model/Sensor.java
@@ -1,6 +1,9 @@
 package dev.williancorrea.sensors.device.management.domain.model;
 
-import io.hypersistence.tsid.TSID;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +13,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
 public class Sensor {
-  
-  private TSID id;
+
+  @Id
+  @AttributeOverride(name = "value", column = @Column(name = "id", columnDefinition = "BIGINT"))
+  private SensorId id;
+
   private String name;
   private String ip;
   private String location;
