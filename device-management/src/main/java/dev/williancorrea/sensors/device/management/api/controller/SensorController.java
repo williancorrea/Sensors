76b7b849b2f21b6ff12b539d943616bed337b8fa
Willian Vagner Vicente Corrêa <willian.vag@gmail.com>
2025-12-09 10:14:12 -0300

    Add update and delete endpoints in `SensorController`
    - Introduced `update` and `delete` methods to handle sensor updates and deletions.
    - Applied `@Valid` and validation constraints (`@NotBlank`) to `SensorInput` fields.
    - Added sample HTTP templates for PUT and DELETE requests.
    - Updated dependencies to include `spring-boot-starter-validation`.

--- device-management/src/main/java/dev/williancorrea/sensors/device/management/api/controller/SensorController.java
+++ device-management/src/main/java/dev/williancorrea/sensors/device/management/api/controller/SensorController.java
@@ -7,14 +7,17 @@ import dev.williancorrea.sensors.device.management.domain.model.Sensor;
 import dev.williancorrea.sensors.device.management.domain.model.SensorId;
 import dev.williancorrea.sensors.device.management.domain.repository.SensorRepository;
 import io.hypersistence.tsid.TSID;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -36,14 +39,13 @@ public class SensorController {
 
   @GetMapping("/{sensorId}")
   public SensorOutput getOne(@PathVariable("sensorId") TSID sensorId) {
-    Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
-        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Sensor not found"));
+    Sensor sensor = findById(sensorId);
     return new SensorOutput(sensor);
   }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public SensorOutput create(@RequestBody SensorInput input) {
+  public SensorOutput create(@Valid @RequestBody SensorInput input) {
     Sensor sensor = Sensor.builder()
         .id(new SensorId(IdGenerator.generateTSID()))
         .name(input.getName())
@@ -55,5 +57,32 @@ public class SensorController {
         .build();
 
     return new SensorOutput(sensorRepository.saveAndFlush(sensor));
+  }
+
+  @PutMapping("/{sensorId}")
+  public SensorOutput update(@PathVariable("sensorId") TSID sensorId,
+                             @Valid @RequestBody SensorInput input) {
+    Sensor sensor = findById(sensorId);
+
+    sensor.setName(input.getName());
+    sensor.setIp(input.getIp());
+    sensor.setLocation(input.getLocation());
+    sensor.setProtocol(input.getProtocol());
+    sensor.setModel(input.getModel());
+
+    return new SensorOutput(sensorRepository.saveAndFlush(sensor));
+  }
+
+  @DeleteMapping("/{sensorId}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void update(@PathVariable("sensorId") TSID sensorId) {
+    Sensor sensor = findById(sensorId);
+    sensorRepository.delete(sensor);
+  }
+
+
+  private Sensor findById(TSID sensorId) {
+    return sensorRepository.findById(new SensorId(sensorId))
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Sensor not found"));
   }
 }
--- device-management/src/main/java/dev/williancorrea/sensors/device/management/api/model/SensorInput.java
+++ device-management/src/main/java/dev/williancorrea/sensors/device/management/api/model/SensorInput.java
@@ -1,12 +1,19 @@
 package dev.williancorrea.sensors.device.management.api.model;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class SensorInput {
+
+  @NotBlank
   private String name;
+  @NotBlank
   private String ip;
+  @NotBlank
   private String location;
+  @NotBlank
   private String protocol;
+  @NotBlank
   private String model;
 }
