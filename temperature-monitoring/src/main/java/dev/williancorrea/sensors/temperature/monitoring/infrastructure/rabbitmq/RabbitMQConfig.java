a00fc67644166b4d70504fd8cabb6bbe7dea86e8
Willian Vagner Vicente Corrêa <willian.vag@gmail.com>
2025-12-17 10:16:54 -0300

    Add dead-letter queue for RabbitMQ temperature processing
    - Enhanced `RabbitMQConfig` to include a dead-letter queue and routing configurations.
    - Updated `docker-compose.yml` to mount `enabled_plugins` for RabbitMQ management plugins.
    - Added `enabled_plugins` file to enable RabbitMQ shovel and management plugins.

--- temperature-monitoring/src/main/java/dev/williancorrea/sensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQConfig.java
+++ temperature-monitoring/src/main/java/dev/williancorrea/sensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -1,6 +1,7 @@
 package dev.williancorrea.sensors.temperature.monitoring.infrastructure.rabbitmq;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.ExchangeBuilder;
@@ -17,14 +18,18 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
   public static final String EXCHANGE = "temperature-processing.temperature-received.v1.e";
-  public static final String QUEUE_PROCESSING_TEMPERATURE = "temperature-monitoring.process-temperature.v1.q";
+
+  public static final String PROCESSING_TEMPERATURE = "temperature-monitoring.process-temperature.v1";
+  public static final String QUEUE_PROCESSING_TEMPERATURE = PROCESSING_TEMPERATURE + ".q";
+  public static final String DEAD_LETTER_QUEUE_PROCESSING_TEMPERATURE = PROCESSING_TEMPERATURE + ".dlq";
+
   public static final String QUEUE_ALERTING = "temperature-monitoring.alerting.v1.q";
 
   @Bean
   public Jackson2JsonMessageConverter messageConverter(ObjectMapper objectMapper) {
     return new Jackson2JsonMessageConverter(objectMapper);
   }
-  
+
   @Bean
   public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
     return new RabbitAdmin(connectionFactory);
@@ -36,15 +41,25 @@ public class RabbitMQConfig {
         .build();
   }
 
-  
-
   @Bean
   public Queue queueProcessingTemperature() {
+    var args = new HashMap<String, Object>();
+    args.put("x-dead-letter-exchange", "");
+    args.put("x-dead-letter-routing-key", DEAD_LETTER_QUEUE_PROCESSING_TEMPERATURE);
+
     return QueueBuilder
         .durable(QUEUE_PROCESSING_TEMPERATURE)
+        .withArguments(args)
         .build();
   }
-  
+
+  @Bean
+  public Queue deadLetterQueueProcessingTemperature() {
+    return QueueBuilder
+        .durable(DEAD_LETTER_QUEUE_PROCESSING_TEMPERATURE)
+        .build();
+  }
+
   @Bean
   public Binding bindingProcessingTemperature() {
     return BindingBuilder
